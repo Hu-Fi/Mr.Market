59a6082897b4962d1374111a9dc694dbcce879ff
zedwong <36271125+zed-wong@users.noreply.github.com>
2024-01-14 18:22:37 +0700

    Improve add search history logic

--- interface/src/lib/components/skeleton/home/balanceNumberLoader.svelte
+++ interface/src/lib/components/skeleton/home/balanceNumberLoader.svelte
@@ -1,4 +1,4 @@
 <script lang="ts">
 </script>
 
-<div class="h-8 w-28 skeleton rounded-2xl" />
+<div class="h-6 w-28 skeleton rounded-2xl" />
--- interface/src/lib/helpers/searchHistory.ts
+++ interface/src/lib/helpers/searchHistory.ts
@@ -9,16 +9,16 @@ export const addSearchHistory = (item: object) => {
   let history = getSearchHistory()
   // console.log('history:', history)
   
-  if (history.length > 6) {
-    history.shift();
-  }
-  
-  let existingItem = history.find(h => h.symbol === item.symbol);
-  if (!existingItem) {
+  let index = history.findIndex(h => h.symbol === item.symbol);
+  if (index === -1) {
     history.push(item);
   } else {
-    history = history.filter(h => h.symbol !== item.symbol);
-    history.push(item)
+    let existingItem = history.splice(index, 1)[0];
+    history.push(existingItem);
+  }
+
+  if (history.length > 6) {
+    history.shift();
   }
   localStorage.setItem('search-history', JSON.stringify(history))
 }
