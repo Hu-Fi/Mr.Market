36504941c0aa389eb1968066c9ae618cdc7758e2
zedwong <36271125+zed-wong@users.noreply.github.com>
2024-01-28 18:20:45 +0700

    Fix playwright webServer

--- interface/playwright.config.ts
+++ interface/playwright.config.ts
@@ -78,7 +78,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'bun dev',
+    command: 'npm run dev',
     url: 'http://127.0.0.1:5173',
     reuseExistingServer: !process.env.CI,
   },
