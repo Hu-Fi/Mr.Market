17c53beaee5981ae8a37a7d4d9181656d896c78f
zedwong <36271125+zed-wong@users.noreply.github.com>
2024-03-10 08:59:26 +0700

    Fix all playwright tests

--- interface/playwright.config.ts
+++ interface/playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 3000,
+  timeout: 5000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -82,5 +82,6 @@ export default defineConfig({
     command: 'npm run dev',
     url: 'http://127.0.0.1:5173',
     reuseExistingServer: true,
+    ignoreHTTPSErrors: true,
   },
 });
