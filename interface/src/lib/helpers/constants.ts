0dfe6414347c20899b55ab3b40c8a621e88bd76c
zedwong <36271125+zed-wong@users.noreply.github.com>
2024-03-12 23:07:44 +0700

    Modify backend url

--- interface/src/lib/helpers/constants.ts
+++ interface/src/lib/helpers/constants.ts
@@ -10,8 +10,8 @@ export const OAUTH_SCOPE = env.PUBLIC_OAUTH_SCOPE || 'PROFILE:READ ASSETS:READ S
 export const MIXIN_MESSENGER_INSTALL = env.PUBLIC_MIXIN_MESSENGER_INSTALL || 'https://messenger.mixin.one/install'
 export const MIXIN_API_BASE_URL = env.PUBLIC_MIXIN_API_BASE_URL || 'https://api.mixin.one'
 
-export const HUFI_SOCKET_URL = env.PUBLIC_HUFI_SOCKET_URL || '//bc6e1fa0-3c5a-4235-809c-c4fcc4a5d859.mvg.fi'
-export const HUFI_BACKEND_URL = env.PUBLIC_HUFI_BACKEND_URL || 'https://bc6e1fa0-3c5a-4235-809c-c4fcc4a5d859.mvg.fi:3000'
+export const HUFI_SOCKET_URL = env.PUBLIC_HUFI_SOCKET_URL || '//mrmarket-pr.up.railway.app/'
+export const HUFI_BACKEND_URL = env.PUBLIC_HUFI_BACKEND_URL || 'https://mrmarket-pr.up.railway.app/'
 export const HUMAN_PROTOCOL_GROUP_URL = env.PUBLIC_HUMAN_PROTOCOL_GROUP_URL || 'https://mixin.one/apps/5a33fc52-f445-4170-a06a-47f8be94a8f3'
 
 export const SUPPORTED_PAIRS: {
