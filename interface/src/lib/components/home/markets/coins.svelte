eb0db351140fd1c9b3bb5b8685961d7631610bab
zedwong <36271125+zed-wong@users.noreply.github.com>
2024-03-16 12:17:09 +0700

    Add filterLoader

--- interface/src/lib/components/home/markets/coins.svelte
+++ interface/src/lib/components/home/markets/coins.svelte
@@ -7,6 +7,7 @@
   import TableColumns from "$lib/components/home/markets/tableColumns.svelte";
   import SingleTokenLoader from "$lib/components/skeleton/market/singleTokenLoader.svelte";
     import { marketData } from "$lib/stores/market";
+    import FilterLoader from "$lib/components/skeleton/market/filterLoader.svelte";
 
   let defaults = [
     {"id":"bitcoin","symbol":"btc","name":"Bitcoin","image":"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400","current_price":42632,"market_cap":835055181228,"market_cap_rank":1,"fully_diluted_valuation":895305808841,"total_volume":12859406774,"high_24h":42782,"low_24h":42095,"price_change_24h":52.9,"price_change_percentage_24h":0.12424,"market_cap_change_24h":2568819209,"market_cap_change_percentage_24h":0.30857,"circulating_supply":19586781,"total_supply":21000000,"max_supply":21000000,"ath":69045,"ath_change_percentage":-38.2734,"ath_date":"2021-11-10T14:24:11.849Z","atl":67.81,"atl_change_percentage":62751.51889,"atl_date":"2013-07-06T00:00:00.000Z","roi":null,"last_updated":"2024-01-01T15:12:29.021Z"}, 
@@ -37,6 +38,9 @@
 
 <div class="w-full mb-24">
   {#if !resolved}
+    <div class="w-full mx-4">
+      <FilterLoader />
+    </div>
     <!-- Loading -->
     {#each Array(12) as _}
       <SingleTokenLoader />
--- interface/src/lib/components/skeleton/market/filterLoader.svelte
+++ interface/src/lib/components/skeleton/market/filterLoader.svelte
@@ -1,26 +1,23 @@
-<thead>
-  <tr class="flex justify-between border-b-0">
-    <th class="cursor-pointer pb-2 md:first:pl-7 md:last:pr-7">
+<div>
+  <div class="flex justify-between border-b-0">
+    <div class="cursor-pointer pb-2">
       <button class="flex skeleton h-3 w-10 items-center">
         <div class="text-xs !text-[10px]" />
-        <div class="flex flex-col space-y-[-8px]" />
       </button>
-    </th>
+    </div>
 
-    <div>
-      <th class="cursor-pointer pb-2 md:first:pl-7 md:last:pr-7">
-        <button class="flex skeleton h-3 w-10 items-center mr-4">
-          <div class="text-xs !text-[10px]" />
-          <div class="flex flex-col space-y-[-8px]" />
-        </button>
-      </th>
-  
-      <th class="cursor-pointer pb-2 md:first:pl-7 md:last:pr-7">
+    <div class="flex space-x-4 mr-10">
+      <div class="cursor-pointer pb-2 mr-8">
         <button class="flex skeleton h-3 w-10 items-center">
           <div class="text-xs !text-[10px]" />
-          <div class="flex flex-col space-y-[-8px]" />
         </button>
-      </th>
+      </div>
+  
+      <div class="cursor-pointer pb-2">
+        <button class="flex skeleton h-3 w-10 items-center">
+          <div class="text-xs !text-[10px]" />
+        </button>
+      </div>
     </div>
-  </tr>
-</thead>
+  </div>
+</div>
