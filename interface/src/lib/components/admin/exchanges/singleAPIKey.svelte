d3e1f866827dccf4bad374d710d02a8c31f6a4e1
zedwong <36271125+zed-wong@users.noreply.github.com>
2024-03-18 20:07:18 +0800

    Add delete api key ui

--- interface/src/lib/components/admin/exchanges/singleAPIKey.svelte
+++ interface/src/lib/components/admin/exchanges/singleAPIKey.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+  import { _ } from "svelte-i18n";
   import type { AdminSingleKey } from "$lib/types/hufi/admin";
   import { findExchangeIconByIdentifier } from "$lib/helpers/helpers";
   
   export let key: AdminSingleKey;
+  let deleteConfirm = false;
+
+  const deleteAPIKey = (key_id: string) => {
+    console.log('delete api key')
+    deleteConfirm = false;
+  }
 </script>
 
 <div class="flex items-center justify-between p-4 border-b border-x first:border-t last:border-b">
@@ -18,14 +25,38 @@
     </div>
   </div>
   
-  <div class="flex flex-col items-end justify-center space-y-1">
-    {#if key.state === 'alive'}
-      <div class="badge badge-primary badge-outline capitalize">{key.state}</div>
-    {:else}
-      <div class="badge badge-error badge-outline capitalize">{key.state}</div>
-    {/if}
-    <div>
-      <span class="text-xs opacity-60"> {key.last_update} </span>
+  <div class="flex space-x-8">
+    <!-- Actions -->
+    <div class="flex items-center justify-center">
+      {#if !deleteConfirm}
+        <button class="btn btn-sm" on:click={()=>{deleteConfirm=true}}>
+          <span>{$_('delete')}</span>
+        </button>
+      {:else}
+        <div class="flex space-x-4 items-center">
+          <button class="flex items-center justify-center rounded-full p-0 h-5 w-5" on:click={()=>deleteConfirm=false}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+          </button>
+
+          <button class="flex items-center justify-center rounded-full p-0 h-5 w-5" on:click={()=>deleteAPIKey(key.key)}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+            </svg>              
+          </button>
+        </div>
+      {/if}
+    </div>
+    <div class="flex flex-col items-end justify-center space-y-1">
+      {#if key.state === 'alive'}
+        <div class="badge badge-primary badge-outline capitalize">{key.state}</div>
+      {:else}
+        <div class="badge badge-error badge-outline capitalize">{key.state}</div>
+      {/if}
+      <div>
+        <span class="text-xs opacity-60"> {key.last_update} </span>
+      </div>
     </div>
   </div>
 </div>
