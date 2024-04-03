16d97897fe5d4f8a617766da24133f0525f68f33
Mariusz Jasuwienas <mariusz.jasuwienas@arianelabs.com>
2024-04-03 19:31:28 +0200

    74

--- interface/src/routes/(bottomNav)/(market)/market/token/+page.svelte
+++ interface/src/routes/(bottomNav)/(market)/market/token/+page.svelte
@@ -1,21 +1,17 @@
 <script lang="ts">
   import { onDestroy } from "svelte";
-  import { page } from "$app/stores";
   import { sortCoins } from "$lib/helpers/sortTable";
   import type { CoingeckoToken } from "$lib/types/coingecko/token";
-  import {activeSecondTab, asc, marketData, marketDataState, selectedField} from "$lib/stores/market";
+  import { asc, marketData, marketDataState, selectedField } from "$lib/stores/market";
   import SingleToken from "$lib/components/market/elements/singleToken.svelte";
   import TableColumns from "$lib/components/market/elements/tableColumns.svelte";
   import SingleTokenLoader from "$lib/components/skeleton/market/singleTokenLoader.svelte";
   import TokenFilterLoader from "$lib/components/skeleton/market/tokenFilterLoader.svelte";
-  import { marketQueryFn } from "$lib/helpers/hufi/coin";
   import { CoinsTypeTabs } from "$lib/helpers/constants.js";
 
   let tokens: CoingeckoToken[] = $marketData || [];
-
   $: sortedTokens = tokens.length != 0 ? sortCoins($selectedField, tokens, $asc) : [];
-
-  let resolved = $marketDataState !== 'loading';
+  $: resolved = $marketDataState !== 'loading';
 </script>
 
 <div class="flex flex-col">
