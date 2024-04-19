bfbbff9632f39e73dce44de7afaa07abf0ab78b1
zedwong <36271125+zed-wong@users.noreply.github.com>
2024-04-19 16:48:07 +0700

    avoid using api key for market data

--- server/src/modules/marketdata/marketdata.service.ts
+++ server/src/modules/marketdata/marketdata.service.ts
@@ -25,48 +25,12 @@ export class MarketdataService {
 
   private initializeExchange() {
     // Initialize exchanges
-    this.exchanges.set(
-      'bitfinex',
-      new ccxt.pro.bitfinex({
-        apiKey: process.env.BITFINEX_API_KEY,
-        secret: process.env.BITFINEX_SECRET,
-      }),
-    );
-    this.exchanges.set(
-      'mexc',
-      new ccxt.pro.mexc({
-        apiKey: process.env.MEXC_API_KEY,
-        secret: process.env.MEXC_SECRET,
-      }),
-    );
-    this.exchanges.set(
-      'binance',
-      new ccxt.pro.binance({
-        apiKey: process.env.BINANCE_API_KEY,
-        secret: process.env.BINANCE_SECRET,
-      }),
-    );
-    this.exchanges.set(
-      'okx',
-      new ccxt.pro.okx({
-        apiKey: process.env.OKX_API_KEY,
-        secret: process.env.OKX_SECRET,
-      }),
-    );
-    this.exchanges.set(
-      'gate',
-      new ccxt.pro.gateio({
-        apiKey: process.env.GATE_API_KEY,
-        secret: process.env.GATE_SECRET,
-      }),
-    );
-    this.exchanges.set(
-      'lbank',
-      new ccxt.pro.lbank({
-        apiKey: process.env.LBANK_API_KEY,
-        secret: process.env.LBANK_SECRET,
-      }),
-    );
+    this.exchanges.set('bitfinex', new ccxt.pro.bitfinex());
+    this.exchanges.set('mexc', new ccxt.pro.mexc());
+    this.exchanges.set('binance', new ccxt.pro.binance());
+    this.exchanges.set('okx', new ccxt.pro.okx());
+    this.exchanges.set('gate', new ccxt.pro.gateio());
+    this.exchanges.set('lbank', new ccxt.pro.lbank());
   }
 
   async getTickers(exchange: string, symbols: string[]) {
