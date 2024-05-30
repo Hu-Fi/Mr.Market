df4a5dcf58e1258d9c7fd181271600da822eb25c
zedwong <36271125+zed-wong@users.noreply.github.com>
2024-05-30 14:02:43 +0800

    add just grow intro

--- interface/src/lib/components/grow/elements/entrance.svelte
+++ interface/src/lib/components/grow/elements/entrance.svelte
@@ -22,7 +22,7 @@
         }
       }
     >
-      <div class="flex w-full justify-between">
+      <div class="flex w-full justify-between items-center">
         <div class="flex flex-row -space-x-1">
           <div class={clsx("flex items-center justify-center rounded-full p-2 border")}>
             <!-- Arbitrage -->
@@ -42,8 +42,11 @@
           </div>
         </div>
         
-        <div class="flex items-center justify-center text-end">
-          <span class="badge badge-sm badge-accent badge-outline">
+        <div class="flex items-center justify-center text-end badge badge-sm badge-accent badge-outline">
+          <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+          </svg>           -->
+          <span class="">
             {$_('recommended')}
           </span>
         </div>
---
+++ interface/src/lib/components/grow/justGrow/intro/details.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { _ } from "svelte-i18n"
+</script>
+
+<div class="flex flex-col space-y-10 mt-4">
+  <div class="flex flex-col space-y-4">
+    <div class="px-4 text-lg font-bold">
+      <span> {$_('whats_just_grow')} </span>
+    </div>
+    <div class="px-4 text-sm text-[10px] text-base-content/60">
+      <span> {$_('whats_just_grow_answer')} </span>
+    </div>
+  </div>
+  
+  <div class="flex flex-col space-y-4">
+    <div class="px-4 text-lg font-bold">
+      <span> {$_('what_re_benefits')} </span>
+    </div>
+    <div class="flex flex-col space-y-2">
+      <div class="px-4 text-sm font-bold">
+        <span> {$_('easy_to_participate')} </span>
+      </div>
+      <div class="px-4 text-sm text-[10px] text-base-content/60">
+        <span> {$_('easy_to_participate_detail')} </span>
+      </div>
+    </div>
+    <div class="flex flex-col space-y-2">
+      <div class="px-4 text-sm font-bold">
+        <span> {$_('no_fees')} </span>
+      </div>
+      <div class="px-4 text-sm text-[10px] text-base-content/60">
+        <span> {$_('no_fees_detail')} </span>
+      </div>
+    </div>
+  </div>
+</div>
