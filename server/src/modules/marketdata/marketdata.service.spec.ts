03492e46c10a8632023e797cbae42c7fb3bc88f5
Faouzijedidi1 <faouzijedidi1@gmail.com>
2024-07-05 18:16:06 +0400

    Fix tests

--- server/src/modules/marketdata/marketdata.service.spec.ts
+++ server/src/modules/marketdata/marketdata.service.spec.ts
@@ -1,8 +1,8 @@
-// marketdata.service.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
 import { MarketdataService } from './marketdata.service';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { CustomLogger } from '../logger/logger.service';
+import { ExchangeInitService } from '../exchangeInit/exchangeInit.service';
 
 jest.mock('../logger/logger.service');
 
@@ -10,6 +10,7 @@ const mockCacheManager = () => ({
   get: jest.fn(),
   set: jest.fn(),
 });
+
 // Mock setup for ccxt
 const mockFetchTickers = jest.fn();
 const mockFetchOHLCV = jest.fn();
@@ -33,21 +34,29 @@ jest.mock('ccxt', () => ({
   },
 }));
 
+const mockExchangeInitService = () => ({
+  getExchange: jest.fn(),
+  getSupportedExchanges: jest.fn(),
+});
+
 describe('MarketdataService', () => {
   let service: MarketdataService;
   let cacheManager;
+  let exchangeInitService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         MarketdataService,
         { provide: CACHE_MANAGER, useFactory: mockCacheManager },
+        { provide: ExchangeInitService, useFactory: mockExchangeInitService },
         CustomLogger,
       ],
     }).compile();
 
     service = module.get<MarketdataService>(MarketdataService);
     cacheManager = module.get(CACHE_MANAGER);
+    exchangeInitService = module.get<ExchangeInitService>(ExchangeInitService);
   });
 
   afterEach(() => {
@@ -58,6 +67,11 @@ describe('MarketdataService', () => {
     it('fetches tickers successfully from a supported exchange', async () => {
       const expectedTickers = { BTCUSD: { last: 50000 } };
       mockFetchTickers.mockResolvedValue(expectedTickers);
+      exchangeInitService.getExchange.mockReturnValue({
+        fetchTickers: mockFetchTickers,
+        has: { fetchTickers: true },
+        name: 'binance',
+      });
 
       const tickers = await service.getTickers('binance', ['BTCUSD']);
       expect(tickers).toEqual(expectedTickers);
@@ -66,6 +80,11 @@ describe('MarketdataService', () => {
 
     it('throws an error when fetchTickers fails', async () => {
       mockFetchTickers.mockRejectedValue(new Error('API call failed'));
+      exchangeInitService.getExchange.mockReturnValue({
+        fetchTickers: mockFetchTickers,
+        has: { fetchTickers: true },
+        name: 'binance',
+      });
 
       await expect(service.getTickers('binance', ['BTCUSD'])).rejects.toThrow(
         'API call failed',
@@ -77,6 +96,11 @@ describe('MarketdataService', () => {
     it('fetches OHLCV data successfully', async () => {
       const expectedOHLCV = [[1609459200000, 29000, 29500, 28500, 29300, 1200]];
       mockFetchOHLCV.mockResolvedValue(expectedOHLCV);
+      exchangeInitService.getExchange.mockReturnValue({
+        fetchOHLCV: mockFetchOHLCV,
+        has: { fetchOHLCV: true },
+        name: 'binance',
+      });
 
       const OHLCV = await service.getOHLCVData('binance', 'BTCUSD');
       expect(OHLCV).toEqual(
@@ -97,6 +121,7 @@ describe('MarketdataService', () => {
       );
     });
   });
+
   describe('getSupportedPairs', () => {
     it('returns supported pairs from cache if available', async () => {
       const cachedPairs = [{ symbol: 'BTCUSD', price: 50000 }];
