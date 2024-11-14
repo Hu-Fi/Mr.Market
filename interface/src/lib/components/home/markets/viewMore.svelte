93e48ccc391dd11b3da33b819bc5159cc7c00972
Zed Wong <kingflyme1@gmail.com>
2024-11-14 14:54:41 +0400

    fix viewmore button hover effect

--- interface/src/lib/components/home/markets/viewMore.svelte
+++ interface/src/lib/components/home/markets/viewMore.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex justify-center w-full p-4">
     <button
-        class="btn btn-outline border-base-300 w-full rounded-full no-animation"
+        class="btn btn-outline border-base-300 w-full rounded-full no-animation hover:bg-base-100 hover:border-base-300 focus:bg-base-100"
         on:click={() => goto("/market")}
     >
         <span class="mx-3 text-sm text-base-content/80">{$_("view_more")}</span>
