f782d1b7c1974542e33fb25b08e16c8030231b8e
Zed Wong <kingflyme1@gmail.com>
2024-12-11 17:32:24 +0400

    update migrations

---
+++ server/src/database/migrations/1730464574077-strategyEntity.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class StrategyEntity1730464574077 implements MigrationInterface {
+  name = 'StrategyEntity1730464574077';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            CREATE TABLE "strategy_instances" (
+                "id" SERIAL NOT NULL,
+                "strategyKey" character varying NOT NULL,
+                "userId" character varying NOT NULL,
+                "clientId" character varying NOT NULL,
+                "strategyType" character varying NOT NULL,
+                "parameters" json NOT NULL,
+                "status" character varying NOT NULL,
+                "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+                "updatedAt" TIMESTAMP NOT NULL DEFAULT now(),
+                CONSTRAINT "PK_09080dbac0815101d4b0e883e7a" PRIMARY KEY ("id")
+            )
+        `);
+    await queryRunner.query(`
+            CREATE TABLE "contribution" (
+                "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "amount" numeric(18, 8) NOT NULL,
+                "joinedAt" TIMESTAMP NOT NULL DEFAULT now(),
+                CONSTRAINT "PK_878330fa5bb34475732a5883d58" PRIMARY KEY ("id")
+            )
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            DROP TABLE "contribution"
+        `);
+    await queryRunner.query(`
+            DROP TABLE "strategy_instances"
+        `);
+  }
+}
