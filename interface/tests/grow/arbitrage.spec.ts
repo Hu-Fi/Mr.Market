a4f605ffb897fcaa7ff051163466fe47cc9db8be
Faouzi JEDIDI <faouzijedidi1@gmail.com>
2025-09-25 17:39:24 +0400

    fix tests: remove redundant test.use

--- interface/tests/grow/arbitrage.spec.ts
+++ interface/tests/grow/arbitrage.spec.ts
@@ -1,8 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.use({
-  viewport: { width: 390, height: 844 },  // iPhone 14 Pro
-});
+
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://127.0.0.1:5173/grow');
--- interface/tests/spot/candlestick.spec.ts
+++ interface/tests/spot/candlestick.spec.ts
@@ -1,8 +1,5 @@
 import { expect, test } from '@playwright/test';
 
-test.use({
-  viewport: { width: 390, height: 844 },  // iPhone 14 Pro
-});
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://127.0.0.1:5173/market/candle/okx/BTC-USDT');
