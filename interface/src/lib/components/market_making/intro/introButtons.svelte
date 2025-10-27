d7a024efdabf5d39affaffc5a5f295282eabec56
Zed Wong <kingflyme1@gmail.com>
2025-10-27 16:00:36 +0800

    add buttons in mm page

--- interface/src/lib/components/market_making/intro/introButtons.svelte
+++ interface/src/lib/components/market_making/intro/introButtons.svelte
@@ -1,5 +1,13 @@
-<div class="mx-8">
-  <button class="btn bg-base-content text-base-100 w-full rounded-lg no-animation btn-circle">
-    Get Started
+<script>
+  import { _ } from "svelte-i18n";
+</script>
+
+<div class="mx-8 flex flex-col space-y-4">
+  <button class="btn bg-base-content text-base-100 w-full rounded-lg no-animation">
+    {$_('start_market_making')}
+  </button>
+
+  <button class="btn bg-base-100 text-base-content w-full rounded-lg no-animation">
+    {$_('learn_more')}
   </button>
 </div>
