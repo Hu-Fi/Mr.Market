a01d8c842426a362004fa2d46405ccb7062a0f56
Zed Wong <kingflyme1@gmail.com>
2025-11-24 17:43:46 +0800

    fix ui img alt

--- interface/src/routes/(bottomNav)/(market-making)/market-making/hufi/learn-more/+page.svelte
+++ interface/src/routes/(bottomNav)/(market-making)/market-making/hufi/learn-more/+page.svelte
@@ -11,36 +11,43 @@
   <div class="carousel-item h-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+      alt=""
     />
   </div>
   <div class="carousel-item h-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+      alt=""
     />
   </div>
   <div class="carousel-item h-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+      alt=""
     />
   </div>
   <div class="carousel-item h-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+      alt=""
     />
   </div>
   <div class="carousel-item h-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
+      alt=""
     />
   </div>
   <div class="carousel-item h-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+      alt=""
     />
   </div>
   <div class="carousel-item h-full">
     <img
       src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+      alt=""
     />
   </div>
 </div>
