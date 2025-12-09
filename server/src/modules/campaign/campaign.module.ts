88e38828cfe3b89bcbc681deaf4c5cf25e1e5609
Zed Wong <kingflyme1@gmail.com>
2025-12-09 21:39:44 +0800

    remove unused httpModule

--- server/src/modules/campaign/campaign.module.ts
+++ server/src/modules/campaign/campaign.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 import { CampaignService } from './campaign.service';
-import { HttpModule } from '@nestjs/axios';
 import { Web3Module } from '../web3/web3.module';
 import { ExchangeInitModule } from '../infrastructure/exchange-init/exchange-init.module';
 
 @Module({
-  imports: [HttpModule, Web3Module, ExchangeInitModule],
+  imports: [Web3Module, ExchangeInitModule],
   providers: [CampaignService, ConfigService],
   exports: [CampaignService],
 })
-export class CampaignModule {}
+export class CampaignModule { }
--- server/src/modules/mixin/mixin.module.ts
+++ server/src/modules/mixin/mixin.module.ts
@@ -1,20 +1,18 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { MessageModule } from './message/message.module';
 import { SnapshotsModule } from './snapshots/snapshots.module';
-import { RebalanceModule } from './rebalance/rebalance.module';
+// import { RebalanceModule } from './rebalance/rebalance.module';
 import { ExchangeModule } from './exchange/exchange.module';
 import { UserModule } from './user/user.module';
 import { WithdrawalModule } from '../market-making/withdrawal/withdrawal.module';
-import { HttpModule } from '@nestjs/axios'; // Added this import
 
 @Module({
   imports: [
-    HttpModule,
     SnapshotsModule,
     ExchangeModule,
     MessageModule,
     UserModule,
-    RebalanceModule,
+    // RebalanceModule,
     forwardRef(() => WithdrawalModule),
   ],
   exports: [
